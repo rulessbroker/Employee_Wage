2d758e5ddb86cf25c474763f3046e815507c7504
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-11-11 15:23:35 +0530

    Added: Part-Time wage of Employee

--- src/EmpWageComputation.java
+++ src/EmpWageComputation.java
@@ -4,15 +4,21 @@ public class EmpWageComputation {
 
 		int wagePerHour = 20;
 		int fullTimeHour = 8;
+		int partTimeHour = 4;
 
 		System.out.println("***Welcome to Employee Wage Computation Program***\n");
 
-		int check = (int) ((Math.random() * 10) % 2);
+		int check = (int) ((Math.random() * 10) % 3);
+
+		int dailyWage = wagePerHour * fullTimeHour;
+		int partTimeWage = wagePerHour * (fullTimeHour + partTimeHour);
 
 		if (check == 1) {
 			System.out.println("Employee is Present");
-			int dailyWage = wagePerHour * fullTimeHour;
 			System.out.println("Daily wage of employee = " + dailyWage);
+		} else if (check == 2) {
+			System.out.println("Employee is Present on Part-Time");
+			System.out.println("Part-Time wage os Employee = " + partTimeWage);
 		} else {
 			System.out.println("Employee is Absent");
 		}
