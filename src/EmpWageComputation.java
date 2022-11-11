e98ae885efabb43530ab9e52f0111b002c696f74
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-11-11 14:02:33 +0530

    Added: Check Employee Attendence

--- src/EmpWageComputation.java
+++ src/EmpWageComputation.java
@@ -1,8 +1,15 @@
 
 public class EmpWageComputation {
 	public static void main(String[] args) {
+
 		System.out.println("***Welcome to Employee Wage Computation Program***\n");
-		
-		
+
+		int check = (int) ((Math.random() * 10) % 2);
+
+		if (check == 1) {
+			System.out.println("Employee is Present");
+		} else {
+			System.out.println("Employee is Absent");
+		}
 	}
 }
