5e4347ebd7074dd967cbdcf794f61f0520e71e63
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-11-11 15:59:07 +0530

    Added: Apply condition till days 20 and hours 80

--- src/EmpWageComputation.java
+++ src/EmpWageComputation.java
@@ -5,30 +5,36 @@ public class EmpWageComputation {
 		int wagePerHour = 20;
 		int fullTimeHour = 8;
 		int partTimeHour = 4;
-		int workingDaysOfMonth = 20;
+		int workingDaysOfMonth = 0;
+		int totalHour = 0;
+		int totalDailyWage = 0;
 
 		System.out.println("***Welcome to Employee Wage Computation Program***\n");
 
-		int check = (int) ((Math.random() * 10) % 3);
+		while (totalHour < 80 && workingDaysOfMonth < 20) {
+			int check = (int) ((Math.random() * 10) % 3);
 
-		int dailyWage = wagePerHour * fullTimeHour;
-		int partTimeWage = wagePerHour * (fullTimeHour + partTimeHour);
-		int wageForMonth = dailyWage * workingDaysOfMonth;
+			workingDaysOfMonth++;
 
-		switch (check) {
-		case 1:
-			System.out.println("Employee is Present");
-			System.out.println("Daily wage of employee = " + dailyWage);
-			System.out.println("Employee present full month and full month wage of employee = " + wageForMonth);
-			break;
-		case 2:
-			System.out.println("Employee is Present on Part-Time");
-			System.out.println("Part-Time wage os Employee = " + partTimeWage);
-			System.out.println("Employee present full month part-time and total vage of employee = "
-					+ (partTimeWage * workingDaysOfMonth));
-			break;
-		default:
-			System.out.println("Employee is Absent");
+			switch (check) {
+			case 1:
+				totalHour = totalHour + fullTimeHour;
+				totalDailyWage = totalDailyWage + fullTimeHour * wagePerHour;
+				System.out.println("Employee is present full time: " + totalDailyWage);
+				break;
+			case 2:
+				totalHour = totalHour + partTimeHour;
+				totalDailyWage = totalDailyWage + partTimeHour * wagePerHour;
+				System.out.println("Employee is present half time: " + totalDailyWage);
+				break;
+			default:
+				System.out.println();
+				System.out.println("Employee is absent: " + totalDailyWage);
+			}
+			System.out.println();
+			System.out.println("Working Days is: " + workingDaysOfMonth + " Hours: " + totalHour);
 		}
+		System.out.println();
+		System.out.println("Employee total wage for month is: " + totalDailyWage);
 	}
 }
