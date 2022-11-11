fb734d24631e5eb1b858d97ab6adf100164f8758
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-11-11 14:11:31 +0530

    Added: Daily wage of Employee

--- src/EmpWageComputation.java
+++ src/EmpWageComputation.java
@@ -2,12 +2,17 @@
 public class EmpWageComputation {
 	public static void main(String[] args) {
 
+		int wagePerHour = 20;
+		int fullTimeHour = 8;
+
 		System.out.println("***Welcome to Employee Wage Computation Program***\n");
 
 		int check = (int) ((Math.random() * 10) % 2);
 
 		if (check == 1) {
 			System.out.println("Employee is Present");
+			int dailyWage = wagePerHour * fullTimeHour;
+			System.out.println("Daily wage of employee = " + dailyWage);
 		} else {
 			System.out.println("Employee is Absent");
 		}
