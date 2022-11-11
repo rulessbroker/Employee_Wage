6f2b10786985e6919f3ad5a08720e9a1a2f6f234
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-11-11 15:38:36 +0530

    Adding: monthly wage of employee

--- src/EmpWageComputation.java
+++ src/EmpWageComputation.java
@@ -5,6 +5,7 @@ public class EmpWageComputation {
 		int wagePerHour = 20;
 		int fullTimeHour = 8;
 		int partTimeHour = 4;
+		int workingDaysOfMonth = 20;
 
 		System.out.println("***Welcome to Employee Wage Computation Program***\n");
 
@@ -12,15 +13,19 @@ public class EmpWageComputation {
 
 		int dailyWage = wagePerHour * fullTimeHour;
 		int partTimeWage = wagePerHour * (fullTimeHour + partTimeHour);
+		int wageForMonth = dailyWage * workingDaysOfMonth;
 
 		switch (check) {
 		case 1:
 			System.out.println("Employee is Present");
 			System.out.println("Daily wage of employee = " + dailyWage);
+			System.out.println("Employee present full month and full month wage of employee = " + wageForMonth);
 			break;
 		case 2:
 			System.out.println("Employee is Present on Part-Time");
 			System.out.println("Part-Time wage os Employee = " + partTimeWage);
+			System.out.println("Employee present full month part-time and total vage of employee = "
+					+ (partTimeWage * workingDaysOfMonth));
 			break;
 		default:
 			System.out.println("Employee is Absent");
